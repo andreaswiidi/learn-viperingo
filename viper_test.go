38729bcc444e5f8f947d0e242f3bdb055ba58109
andreaswiidi <andreaswidi10@gmail.com>
2024-01-27 22:01:33 +0700

    test yaml

--- viper_test.go
+++ viper_test.go
@@ -28,3 +28,19 @@ func TestJSON(t *testing.T) {
 	assert.Equal(t, 3306, config.GetInt("database.port"))
 	assert.Equal(t, true, config.GetBool("database.show_sql"))
 }
+
+func TestYAML(t *testing.T) {
+	config := viper.New()
+	config.SetConfigFile("config.yaml")
+	config.AddConfigPath(".")
+
+	// read config
+	err := config.ReadInConfig()
+	assert.Nil(t, err)
+
+	assert.Equal(t, "belajar-golang-viper", config.GetString("app.name"))
+	assert.Equal(t, "Andreas Widi", config.GetString("app.author"))
+	assert.Equal(t, "localhost", config.GetString("database.host"))
+	assert.Equal(t, 3306, config.GetInt("database.port"))
+	assert.Equal(t, true, config.GetBool("database.show_sql"))
+}
